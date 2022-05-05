8d2cc1fd46387065e95186e2c2d11c277827803e
e <abrandecarlo@protonmail.com>
2022-05-05 02:40:54 -0700

    spend multiple hours automating/researching to save seconds

---
+++ README.md
@@ -0,0 +1,47 @@
+# Bash Script Bazaar
+No need to haggle, tis free
+
+
+## Dir
+```
+src
+└─ createBootable — "Create a bootable drive from an ISO"
+```
+
+## Development
+
+**Setup**
+```
+chmod +x FILENAME.sh
+
+```
+To use anywhere
+```
+1. export PATH=$PATH:~/$dir
+2. source ~/.zshrc || ~/.bashrc || ~/.etc
+3. FILENAME.sh
+
+```
+
+**Using**
+
+#### createBootable
+Create a bootable drive from an ISO
+
+###### Requires sudo
+```
+Commands:
+-p, -path        | ISO file path
+-d, -destination | destination drive
+-h, -help        | HELP
+lsblk            | List all available drives,
+                 | run seperately in terminal
+```
+
+## License
+
+[AGPL-3.0-only](https://github.com/abigger87/femplate/blob/master/LICENSE)
+
+## Disclaimer
+
+_These scripts are being provided as is. No guarantee, representation or warranty is being made, express or implied.  They have not been audited and as such there can be no assurance they will work as intended, and users may experience delays, failures, errors, omissions or loss of transmitted information. The creators are not liable for any of the foregoing. Users should proceed with caution and use at their own risk._
---
+++ createBootable.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+help()
+{
+    echo "Usage:createBootable
+                 | Required Arguments |
+                [ -p | --path ... File path of ISO ]
+                [ -d | --destination ... Destination drive ]
+                [ Output: dd bs=4M if=path of=/dev/destination conv=fdatasync status=progress ]
+                [ lsblk | List all available drives, run seperately in terminal ]
+                "
+    exit 2
+}
+
+SHORT=p:,d:,h
+LONG=path:,destination:,help
+OPTS=$(getopt -a -n createBootable --options $SHORT --longoptions $LONG -- "$@")
+
+VALID_ARGUMENTS=$# # Returns the count of arguments that are in short or long options
+
+if [ "$VALID_ARGUMENTS" -eq 0 ]; then
+  help
+fi
+
+eval set -- "$OPTS"
+
+while :
+do
+  case "$1" in
+    -p | --path )
+      path="$2"
+      shift 2
+      ;;
+    -d | --destination )
+      destination="$2"
+      shift 2
+      ;;
+    -h | --help)
+      help
+      ;;
+    --)
+      shift;
+      break
+      ;;
+    *)
+      echo "Unexpected option: $1"
+      help
+      ;;
+  esac
+done
+
+if [ "$path" ] && [ "$destination" ]
+then
+    dd bs=4M if=$path of=/dev/$destination conv=fdatasync status=progress
+fi
